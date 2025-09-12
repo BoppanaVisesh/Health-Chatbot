881a4b7672337234b89d10385b3b22d12e7d7b1a
visesh boppana <visesh.boppana@gmail.com>
2025-09-12 03:02:52 +0000

    the toolbar  is working slow can u mke it fast it is working after 10 mi

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+      serverActionsTimeout: 60 * 2, // 2 minutes
+    },
+  },
 };
 
 export default nextConfig;
